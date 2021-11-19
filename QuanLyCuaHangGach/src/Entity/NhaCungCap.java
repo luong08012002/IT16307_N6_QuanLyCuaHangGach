ab1f15790f0cc01423cb55fba655439216186485
hoangnvph17059 <73219380+hoangnvph17059@users.noreply.github.com>
2021-11-19 22:00:33 +0700

    VietHoang_Entity_NhaCungCap

---
+++ QuanLyCuaHangGach/src/Entity/NhaCungCap.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author Admin
+ */
+public class NhaCungCap {
+    private String MaNhaCungCap;
+    private String TenNhaCungCap;
+    private String DiaChi;
+    private String SDT;
+    private String ThongTinKhac ;
+    private boolean TrangThai;
+
+    public String getMaNhaCungCap() {
+        return MaNhaCungCap;
+    }
+
+    public void setMaNhaCungCap(String MaNhaCungCap) {
+        this.MaNhaCungCap = MaNhaCungCap;
+    }
+
+    public String getTenNhaCungCap() {
+        return TenNhaCungCap;
+    }
+
+    public void setTenNhaCungCap(String TenNhaCungCap) {
+        this.TenNhaCungCap = TenNhaCungCap;
+    }
+
+    public String getDiaChi() {
+        return DiaChi;
+    }
+
+    public void setDiaChi(String DiaChi) {
+        this.DiaChi = DiaChi;
+    }
+
+    public String getSDT() {
+        return SDT;
+    }
+
+    public void setSDT(String SDT) {
+        this.SDT = SDT;
+    }
+
+    public String getThongTinKhac() {
+        return ThongTinKhac;
+    }
+
+    public void setThongTinKhac(String ThongTinKhac) {
+        this.ThongTinKhac = ThongTinKhac;
+    }
+
+    public boolean isTrangThai() {
+        return TrangThai;
+    }
+
+    public void setTrangThai(boolean TrangThai) {
+        this.TrangThai = TrangThai;
+    }
+
+    @Override
+    public String toString() {
+        return "NhaCungCap{" + "MaNhaCungCap=" + MaNhaCungCap + ", TenNhaCungCap=" + TenNhaCungCap + ", DiaChi=" + DiaChi + ", SDT=" + SDT + ", ThongTinKhac=" + ThongTinKhac + ", TrangThai=" + TrangThai + '}';
+    }
+
+    public NhaCungCap(String MaNhaCungCap, String TenNhaCungCap, String DiaChi, String SDT, String ThongTinKhac, boolean TrangThai) {
+        this.MaNhaCungCap = MaNhaCungCap;
+        this.TenNhaCungCap = TenNhaCungCap;
+        this.DiaChi = DiaChi;
+        this.SDT = SDT;
+        this.ThongTinKhac = ThongTinKhac;
+        this.TrangThai = TrangThai;
+    }
+}

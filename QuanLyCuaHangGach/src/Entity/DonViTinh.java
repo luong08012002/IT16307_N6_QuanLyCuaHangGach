df5d9b0aff630f789521c886df88a8b97980898f
TienDung <94524317+tiendung14540@users.noreply.github.com>
2021-11-20 23:14:14 +0700

    Ducanh entity don vi tinh (#15)
    * Ducanh_entity_DonViTinh
    
    * Create ChatLieu.java
    
    Co-authored-by: ducanhballa <ducanhballa@gmail.com>
    Co-authored-by: NgocSon14 <80674946+NgocSon14@users.noreply.github.com>

--- QuanLyCuaHangGach/src/Entity/DonViTinh.java
+++ QuanLyCuaHangGach/src/Entity/DonViTinh.java
@@ -1,44 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Entity;
 
-/**
- *
- * @author Administrator
- */
+
 public class DonViTinh {
-    private String maChatLieu, tenChatLieu;
+    private String maDonVi, tenDonVi;
 
     public DonViTinh() {
     }
 
-    public DonViTinh(String maChatLieu, String tenChatLieu) {
-        this.maChatLieu = maChatLieu;
-        this.tenChatLieu = tenChatLieu;
+    public DonViTinh(String maDonVi, String tenDonVi) {
+        this.maDonVi = maDonVi;
+        this.tenDonVi = tenDonVi;
     }
 
-    public String getMaChatLieu() {
-        return maChatLieu;
+    public String getMaDonVi() {
+        return maDonVi;
     }
 
-    public void setMaChatLieu(String maChatLieu) {
-        this.maChatLieu = maChatLieu;
+    public void setMaDonVi(String maDonVi) {
+        this.maDonVi = maDonVi;
     }
 
-    public String getTenChatLieu() {
-        return tenChatLieu;
+    public String getTenDonVi() {
+        return tenDonVi;
     }
 
-    public void setTenChatLieu(String tenChatLieu) {
-        this.tenChatLieu = tenChatLieu;
-    }
-
+    public void setTenDonVi(String tenDonVi) {
+        this.tenDonVi = tenDonVi;
+      
     @Override
     public String toString() {
-        return "DonViTinh{" + "maChatLieu=" + maChatLieu + ", tenChatLieu=" + tenChatLieu + '}';
+        return "DonViTinh{" + "maDonVi=" + maDonVi + ", tenDonVi=" + tenDonVi + '}';
     }
     
     

a984db636b12811812e65fccc10418144e37abe1
NgocSon14 <80674946+NgocSon14@users.noreply.github.com>
2021-11-21 16:30:39 +0700

    Sondnph17102_Helper_Image (#31)

---
+++ QuanLyCuaHangGach/src/Helper/Image.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Helper;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Asus
+ */
+public class Image {
+      public static void save(File src) {
+        File dst = new File("logos", src.getName());
+        if (!dst.getParentFile().exists()) {
+            dst.getParentFile().mkdirs();
+        }
+        try {
+            Path from = Paths.get(src.getAbsolutePath());
+            Path to = Paths.get(dst.getAbsolutePath());
+            Files.copy(from, to, StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static ImageIcon read(String fileName) {
+        File path = new File("logos", fileName);
+        return new ImageIcon(new ImageIcon(path.getAbsolutePath()).getImage().getScaledInstance(180, 180, java.awt.Image.SCALE_DEFAULT));
+    }
+}
